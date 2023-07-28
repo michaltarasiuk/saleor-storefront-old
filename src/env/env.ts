0869e92191edf91d4008051a2c07e6617c2e704b
MichalTarasiuk <tarasiuk.michal03@gmail.com>
2023-07-28 17:14:00 +0200

    fix(env): Validation

--- src/env/env.ts
+++ src/env/env.ts
@@ -1,7 +1,5 @@
 import {raise} from '@/tools/raise';
 
-const {env} = process;
-
 export const GRAPHQL_ENDPOINT =
-  env['NEXT_PUBLIC_GRAPHQL_ENDPOINT'] ??
+  process.env['NEXT_PUBLIC_GRAPHQL_ENDPOINT'] ??
   raise('Not found NEXT_PUBLIC_GRAPHQL_ENDPOINT');
