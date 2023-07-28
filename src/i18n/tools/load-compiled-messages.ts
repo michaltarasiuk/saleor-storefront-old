1ef481d273292e7eec7c7337439ff8d658fafb82
luki333 <134687577+luki333@users.noreply.github.com>
2023-07-28 12:38:01 +0200

    fix(i18n): Get default object from module import

--- src/i18n/tools/load-compiled-messages.ts
+++ src/i18n/tools/load-compiled-messages.ts
@@ -2,9 +2,12 @@ import type {AvailableLocle} from '../consts';
 import type {ResolvedIntlConfig} from '@formatjs/intl';
 
 export async function loadCompiledMessages(locale: AvailableLocle) {
-  const compiledMessages: ResolvedIntlConfig['messages'] = await import(
+  const compiledMessagesModule = await import(
     `@/i18n/compiled-messages/${locale}.json`,
     {assert: {type: 'json'}}
   );
+  const compiledMessages: ResolvedIntlConfig['messages'] =
+    compiledMessagesModule.default;
+
   return compiledMessages;
 }
