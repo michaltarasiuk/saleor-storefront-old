bffb4aa027b73dfc4b269f87c4e92329acd47203
Michał Tarasiuk <69385846+MichalTarasiuk@users.noreply.github.com>
2023-07-28 14:06:55 +0200

    feat(components): Add Text (#13)

---
+++ src/components/Text.tsx
@@ -0,0 +1,25 @@
+import {twMerge} from 'tailwind-merge';
+
+type As = keyof Pick<JSX.IntrinsicElements, 'p' | 'span'>;
+
+type Props = {
+  readonly as?: As;
+  readonly size?: 'small' | 'base' | 'large';
+  readonly role?: Extract<JSX.IntrinsicElements[As]['role'], 'alert'>;
+  readonly children: string;
+};
+
+export function Text({as: As = 'p', size = 'base', role, children}: Props) {
+  return (
+    <As
+      role={role}
+      className={twMerge(
+        size === 'small' && 'text-sm',
+        size === 'base' && 'text-base',
+        size === 'large' && 'text-lg',
+        role === 'alert' && 'text-red-600',
+      )}>
+      {children}
+    </As>
+  );
+}
