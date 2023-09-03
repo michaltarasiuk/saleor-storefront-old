04d3d749664d05c25baa991df628d54e77d2dd76
luki333 <134687577+luki333@users.noreply.github.com>
2023-09-03 20:33:57 +0200

    feat-urql (#69)
    * feat(urql): Add helper functions
    
    * feat(urql): Destructure response

--- src/lib/tools/get-client.ts
+++ src/lib/tools/get-client.ts
@@ -1,5 +1,7 @@
+import type {AnyVariables, DocumentInput, OperationContext} from '@urql/core';
 import {createClient, fetchExchange} from '@urql/core';
 import {registerUrql} from '@urql/next/rsc';
+import invariant from 'tiny-invariant';
 
 import {GRAPHQL_ENDPOINT} from '@/env/env-local';
 
@@ -11,3 +13,31 @@ function makeClient() {
 }
 
 export const {getClient} = registerUrql(makeClient);
+
+export async function fetchQueryData<
+  Data,
+  Variables extends AnyVariables = AnyVariables,
+>(
+  query: DocumentInput<Data, Variables>,
+  variables: Variables,
+  context?: Partial<OperationContext>,
+) {
+  const {error, data} = await getClient().query(query, variables, context);
+  invariant(Boolean(error) && data);
+
+  return data;
+}
+
+export async function fetchMutationData<
+  Data,
+  Variables extends AnyVariables = AnyVariables,
+>(
+  query: DocumentInput<Data, Variables>,
+  variables: Variables,
+  context?: Partial<OperationContext>,
+) {
+  const {error, data} = await getClient().mutation(query, variables, context);
+  invariant(Boolean(error) && data);
+
+  return data;
+}
