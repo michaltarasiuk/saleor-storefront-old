96cd6b20c7bf86de5dd2efc400a447238d173830
Michał Tarasiuk <69385846+MichalTarasiuk@users.noreply.github.com>
2023-09-03 15:38:45 +0200

    feat(root): fragment masking config (#67)

--- codegen.ts
+++ codegen.ts
@@ -35,3 +35,32 @@ const codegenConfig: CodegenConfig = {
 };
 
 export default codegenConfig;
+
+// Description: Fragment masking graphql codegen config
+// import type {CodegenConfig} from '@graphql-codegen/cli';
+
+// const codegenConfig: CodegenConfig = {
+//   schema: process.env.NEXT_PUBLIC_GRAPHQL_ENDPOINT,
+//   documents: 'src/**/*.{ts,tsx}',
+//   generates: {
+//     'src/graphql/generated/': {
+//       preset: 'client-preset',
+//       presetConfig: {
+//         fragmentMasking: {unmaskFunctionName: 'getFragment'},
+//       },
+//       config: {
+//         useTypeImports: true,
+//         skipTypename: true,
+//         defaultScalarType: 'unknown',
+//         scalars: {
+//           JSONString: 'string',
+//         },
+//       },
+//     },
+//   },
+//   hooks: {
+//     afterOneFileWrite: ['prettier --w'],
+//   },
+// };
+
+// export default codegenConfig;
