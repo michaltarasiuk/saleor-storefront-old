43bbba434a8f068a50363b8f44df0feefdf53cab
MichalTarasiuk <tarasiuk.michal03@gmail.com>
2024-04-16 09:09:29 +0200

    remove unsued '@ts-ignore'

--- src/i18n/tools/get-canonical-locales.ts
+++ src/i18n/tools/get-canonical-locales.ts
@@ -1,4 +1,3 @@
 export function getCanonicalLocales(locale?: string | readonly string[]) {
-  // @ts-expect-error See: https://github.com/microsoft/TypeScript/issues/29129
   return Intl.getCanonicalLocales(locale);
 }
