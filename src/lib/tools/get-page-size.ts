9fd039f0d1991fea42bc117fa0b183725de3129f
MichalTarasiuk <tarasiuk.michal03@gmail.com>
2024-04-17 13:17:18 +0200

    cover 'getPageSize' utility

--- src/lib/tools/get-page-size.ts
+++ src/lib/tools/get-page-size.ts
@@ -1,4 +1,4 @@
-import type {URLSearchParams} from 'url';
+import {URLSearchParams} from 'url';
 
 import {PAGINATION_SEARCH_PARAM_NAMES} from './get-pagination-search-params';
 import {isDefined} from './is-defined';
@@ -17,4 +17,18 @@ export function getPageSize(
     searchParams.get(PAGINATION_SEARCH_PARAM_NAMES.LAST);
 
   return isDefined(pageSize) ? Number(pageSize) : defaultPageSize;
+}
+
+if (import.meta.vitest) {
+  const {test, expect} = import.meta.vitest;
+
+  test('to be parsed page size', () => {
+    const searchParams = new URLSearchParams([['first', '15']]);
+    expect(getPageSize(searchParams)).toBe(15);
+  });
+
+  test('to be default when page size is not defined', () => {
+    const searchParams = new URLSearchParams();
+    expect(getPageSize(searchParams, 10)).toBe(10);
+  });
 }
