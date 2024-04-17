485649540bfe434e2b17cd408a4c6523b95068d6
MichalTarasiuk <tarasiuk.michal03@gmail.com>
2024-04-17 13:01:02 +0200

    cover 'toggleSearchParam' utility

--- src/lib/tools/toggle-search-param.ts
+++ src/lib/tools/toggle-search-param.ts
@@ -13,3 +13,33 @@ export function toggleSearchParam(
 
   return updatedSearchParams;
 }
+
+if (import.meta.vitest) {
+  const {test, expect} = import.meta.vitest;
+
+  test('to be new search params with selected param', () => {
+    const searchParams = new URLSearchParams();
+    const newSearchParams = toggleSearchParam(
+      searchParams,
+      'category',
+      'audiobooks',
+    );
+
+    expect(newSearchParams).not.toBe(searchParams);
+    expect(Array.from(newSearchParams.entries())).toEqual([
+      ['category', 'audiobooks'],
+    ]);
+  });
+
+  test('to be new search params without selected param', () => {
+    const searchParams = new URLSearchParams([['category', 'audiobooks']]);
+    const newSearchParams = toggleSearchParam(
+      searchParams,
+      'category',
+      'audiobooks',
+    );
+
+    expect(newSearchParams).not.toBe(searchParams);
+    expect(Array.from(newSearchParams.entries())).toEqual([]);
+  });
+}
