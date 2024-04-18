d18f1425c6b0275f07376b645bc0d5688f0ae32d
MichalTarasiuk <tarasiuk.michal03@gmail.com>
2024-04-18 14:23:56 +0200

    split test cases of 'changePageSize' utility

--- src/lib/tools/change-page-size.ts
+++ src/lib/tools/change-page-size.ts
@@ -23,33 +23,27 @@ export function changePageSize(
 if (import.meta.vitest) {
   const {test, expect} = import.meta.vitest;
 
-  test('to be the same search params when page size do not change', () => {
+  test('to be old search params when page size do not change', () => {
     const searchParams = new URLSearchParams([['first', '10']]);
-    const newSearchParams = changePageSize(searchParams, 10);
+    const oldSearchParams = changePageSize(searchParams, 10);
 
-    expect(newSearchParams).toBe(searchParams);
-    expect(Array.from(newSearchParams.entries())).toEqual([['first', '10']]);
+    expect(oldSearchParams).toBe(searchParams);
   });
 
-  test('to be new search params with deleted pagination values and new page size', () => {
-    const searchParams = new URLSearchParams([
-      ['first', '10'],
-      ['after', '5'],
-    ]);
+  test('to be new search params when page size is new one', () => {
+    const searchParams = new URLSearchParams([['first', '10']]);
     const newSearchParams = changePageSize(searchParams, 15);
 
     expect(newSearchParams).not.toBe(searchParams);
-    expect(Array.from(newSearchParams.entries())).toEqual([['first', '15']]);
   });
 
-  test('to be new search params with deleted pagination values and new page size as first', () => {
+  test('to be search params with deleted pagination values and new page size as first', () => {
     const searchParams = new URLSearchParams([
       ['last', '10'],
       ['before', '5'],
     ]);
     const newSearchParams = changePageSize(searchParams, 15);
 
-    expect(newSearchParams).not.toBe(searchParams);
     expect(Array.from(newSearchParams.entries())).toEqual([['first', '15']]);
   });
 }
