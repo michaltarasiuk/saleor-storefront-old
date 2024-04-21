634e33d2cb22a269105ffbac6ea004a9a9f7572f
MichalTarasiuk <tarasiuk.michal03@gmail.com>
2024-04-21 16:05:28 +0200

    cover 'isKeyDefined' utility

--- src/lib/tools/is-key-defined.ts
+++ src/lib/tools/is-key-defined.ts
@@ -8,3 +8,25 @@ export function isKeyDefined<Obj extends UnknownRecord, Key extends keyof Obj>(
 ): obj is Obj & SetNonNullable<SetRequired<Obj, Key>, Key> {
   return Object.hasOwn(obj, key) && isDefined(obj[key]);
 }
+
+if (import.meta.vitest) {
+  /* eslint-disable @typescript-eslint/ban-ts-comment */
+  const {test, expect} = import.meta.vitest;
+
+  test("to be false when object has't own key", () => {
+    // @ts-expect-error
+    expect(isKeyDefined({}, 'foo')).toBeFalsy();
+  });
+
+  test('to be false when value of key is undefined', () => {
+    expect(isKeyDefined({foo: undefined}, 'foo')).toBeFalsy();
+  });
+
+  test('to be false when value of key is null', () => {
+    expect(isKeyDefined({foo: null}, 'foo')).toBeFalsy();
+  });
+
+  test('to be true when value of key is defined', () => {
+    expect(isKeyDefined({foo: 'bar'}, 'foo')).toBeTruthy();
+  });
+}
