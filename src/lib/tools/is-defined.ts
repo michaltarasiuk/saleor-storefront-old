61ca57795817f1d58dee3125dd1ad15963cdfa78
MichalTarasiuk <tarasiuk.michal03@gmail.com>
2024-04-22 19:20:06 +0200

    test(tools): is defined utility

--- src/lib/tools/is-defined.ts
+++ src/lib/tools/is-defined.ts
@@ -1,3 +1,18 @@
 export function isDefined<Value>(value: Value): value is NonNullable<Value> {
   return value !== null && value !== undefined;
 }
+
+if (import.meta.vitest) {
+  const {test, expect} = import.meta.vitest;
+
+  test.each([undefined, null])("to be false when value is '%s'", (value) => {
+    expect(isDefined(value)).toBeFalsy();
+  });
+
+  test.each([true, 3_000, 'Hello World', {}, new Map(), new Set()])(
+    "to be true when value is '%s'",
+    (value) => {
+      expect(isDefined(value)).toBeTruthy();
+    },
+  );
+}
