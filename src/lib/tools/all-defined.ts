9f6c56dd48f1c498d240fc07925369dbc8d863ea
MichalTarasiuk <tarasiuk.michal03@gmail.com>
2024-04-22 19:54:52 +0200

    test(tools): all defined utility

--- src/lib/tools/all-defined.ts
+++ src/lib/tools/all-defined.ts
@@ -3,3 +3,15 @@ import {isDefined} from './is-defined';
 export function allDefined<Value>(...values: readonly Value[]) {
   return values.every((value) => isDefined(value));
 }
+
+if (import.meta.vitest) {
+  const {test, expect} = import.meta.vitest;
+
+  test('to be false when one or more values are not defined', () => {
+    expect(allDefined(1, undefined, 3)).toBeFalsy();
+  });
+
+  test('to be true when all values are defined', () => {
+    expect(allDefined(1, 2, 3)).toBeTruthy();
+  });
+}
