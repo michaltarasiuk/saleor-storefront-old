d6d410573c765b4ddb1c028b388502d03c09f849
MichalTarasiuk <tarasiuk.michal03@gmail.com>
2024-04-22 17:03:11 +0200

    add tests for 'toArray' utility

--- src/lib/tools/to-array.ts
+++ src/lib/tools/to-array.ts
@@ -3,3 +3,16 @@ import {isArray} from './is-array';
 export function toArray<Type>(value: Type | readonly Type[]): readonly Type[] {
   return isArray(value) ? value : [value];
 }
+
+if (import.meta.vitest) {
+  const {test, expect} = import.meta.vitest;
+
+  test('to be the same array when value is type of array', () => {
+    const arr = [1, 2, 3];
+    expect(toArray(arr)).toEqual(arr);
+  });
+
+  test('to be an array with one element when value is not type of array', () => {
+    expect(toArray(null)).toEqual([null]);
+  });
+}
