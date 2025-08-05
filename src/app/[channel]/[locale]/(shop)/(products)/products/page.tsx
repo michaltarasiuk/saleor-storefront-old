56a302763bba9009733a2720fff21ab1e5bba3fa
Michał Tarasiuk <tarasiuk.michal03@gmail.com>
2025-08-05 22:32:48 +0200

    feat(products): add loading skeleton for product list

--- src/app/[channel]/[locale]/(shop)/(products)/products/page.tsx
+++ src/app/[channel]/[locale]/(shop)/(products)/products/page.tsx
@@ -1,5 +1,6 @@
 import {Suspense} from 'react';
 
+import {Skeleton} from '@/lib/components/ui/Skeleton';
 import {cn} from '@/lib/tools/cn';
 import type {SearchParams} from '@/lib/tools/create-search-params';
 import {createSearchParams} from '@/lib/tools/create-search-params';
@@ -28,9 +29,34 @@ async function ProductsPage_({searchParams: searchParamsObj}: Props) {
       <div className={cn('mb-4 mt-2')}>
         <Filters />
       </div>
-      <Suspense fallback="Loading...">
+      <Suspense fallback={<ProductsListSkeleton />}>
         <ProductList key={key} queryVariables={queryVariables} />
       </Suspense>
     </main>
+  );
+}
+
+function ProductsListSkeleton() {
+  return (
+    <ol
+      className={cn(
+        'grid grid-cols-3 gap-x-10 gap-y-6',
+        'max-xl:gap-x-8 max-xl:gap-y-4',
+        'max-lg:grid-cols-2',
+        'max-sm:grid-cols-1',
+      )}>
+      {Array.from({length: 6}, (_, i) => (
+        <li key={i} className={cn('aspect-square')}>
+          <Skeleton className={cn('h-full')} />
+          <div className={cn('mt-2 flex flex-col gap-1')}>
+            <div className={cn('flex items-center justify-between')}>
+              <Skeleton className={cn('h-5 w-28')} />
+              <Skeleton className={cn('h-5 w-20')} />
+            </div>
+            <Skeleton className={cn('h-5 w-20')} />
+          </div>
+        </li>
+      ))}
+    </ol>
   );
 }
